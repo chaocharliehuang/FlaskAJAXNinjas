889a6de2977650677701a289130030bcd62ea173
Chao Charlie Huang <chaocharliehuang@gmail.com>
2017-07-05 19:44:07 -0700

    entering a custom color displays a different img

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,20 @@
-var colors = ['red', 'blue', 'orange', 'purple'];
-
-colors.forEach(function(color) {
-    $('#' + color).click(function() {
-        $.get('/' + color, function(res) {
-            headingHTML = '<h1>' + res.text + '</h1>';
-            $('#heading').html(headingHTML);
-            imgHTML = '<img src="static/imgs/' + res.img + '" alt="ninja" height=300px>';
-            $('#img-div').html(imgHTML);
-        }, 'json');
-    });
+$('.btn').click(function() {
+    var color = $(this).attr('id');
+    displayInfo(color);
 });
+
+$('#form-color').submit(function(e) {
+    var color = $('#customcolor').val();
+    displayInfo(color);
+    $('#customcolor').val('');
+    e.preventDefault();
+});
+
+function displayInfo(color) {
+    $.get('/' + color, function(res) {
+        headingHTML = '<h1>' + res.text + '</h1>';
+        $('#heading').html(headingHTML);
+        imgHTML = '<img src="static/imgs/' + res.img + '" alt="ninja" height=300px>';
+        $('#img-div').html(imgHTML);
+    }, 'json');
+}
